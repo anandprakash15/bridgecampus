e18aee1c9ce50770de1e288f1cbcd8cfcf31b1f0
Chetan Ambelkar <chetan.ambelkar7@gmail.com>
2019-01-13 13:34:47 +0530

    Initial Commit

---
+++ backend/assets/AppAsset.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace backend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main backend application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+        'css/admin.css',
+        'css/skin-black.css',
+        'css/font-awesome/css/font-awesome.css'
+    ];
+    public $js = [
+        'js/admin-main.js',
+        'js/bootstrap.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
